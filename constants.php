34ce8d3b799f20d8bc7440eefae30f5f38c59258
kvsrao <kvsubbaraocse@gmail.com>
2019-01-20 13:13:17 +0530

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,12 +1,3 @@
 <?php
 
-define("KEY", "BharatNext2019KSVRAO");
-define("DB_DRIVER", "mysql");
-define("DB_HOST", "localhost");
-//define("DB_NAME", "bharatne_BharatNext");
-define("DB_NAME", "bharatnext");
-define("DB_USERNAME", "root");
-//define("DB_USERNAME", "bharatne_BharatN");
-//define("DB_PASSWORD", "BharatNext");
-define("DB_PASSWORD", "");
 ?>
